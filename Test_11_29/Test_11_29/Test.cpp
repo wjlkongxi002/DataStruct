c3aa00694e748625bc162741f3e8e442b0c13a21
wjlkongxi002 <wjlkongxi@163.com>
2019-11-30 18:50:36 +0800

    类  11——29

---
+++ Test_11_29/Test_11_29/Test.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+// 1 常方法的const 怎么了
+
+// 2 GetData() 与 GetData()const 能否共存
+
+// 3 拷贝构造函数的const 
+
+// 4 赋值语句 
+
+//   Test& operator=(const Test &t)
+
+//   void  operator=(Test t)
